577fe1b2e35efb42960a254a610ad5644ac4f7b8
williamivan <s32210112@student.ubm.ac.id>
2024-05-24 20:02:55 +0700

    first commit

---
+++ page_about.php
@@ -0,0 +1,14 @@
+<?php include '_header.php'; ?>
+<!-- content -->
+<div class="container mt-5">
+  <div class="card im-box">
+    <h5 class="card-header">About Me</h5>
+    <div class="card-body">
+      <h5 class="card-title">Simple CRUD PHP Native</h5>
+      <p class="card-text">dibuat oleh William Ivan Saputra</p>
+      <a href="page_mahasiswa.php" class="btn btn-primary">Lihat Mahasiswa</a>
+    </div>
+  </div>
+</div>
+<!-- ./content -->
+<?php include '_footer.php'; ?>
